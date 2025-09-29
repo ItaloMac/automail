bcbfd96143b5e233055f9311d442ae7b6aa52a58
italo macedo <macedoitalo21@outlook.com>
2025-09-28 22:00:35 -0300

    refactor:database

--- backend/app/database.py
+++ backend/app/database.py
@@ -6,6 +6,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL não está definido! Configure a variável de ambiente DATABASE_URL.")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
