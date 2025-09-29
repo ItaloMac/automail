b25211936fb5a57645de91074dfcecfa3466e5da
italo macedo <macedoitalo21@outlook.com>
2025-09-29 11:48:39 -0300

    refactor: CORS

--- backend/main.py
+++ backend/main.py
@@ -33,7 +33,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:5173"],
+    allow_origins=["http://localhost:3000", "http://localhost:5173", "https://automail-umber.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],  
     allow_headers=["*"],
